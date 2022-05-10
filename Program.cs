2c8662ff37fc7394b1c1bfe89ccccf48f0689b14
Евгений <@DESKTOP-SJVT4QK>
2022-05-10 13:48:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint age;
+            int money;
+            byte yearsOfStudy;
+            sbyte century;
+            float time;
+            float prise;
+            char schoolClassLetter;
+            string name;
+            string surname;
+            bool enoughMoney;
+
+
+        }
+    }
+}
